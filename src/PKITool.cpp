896fcb990bc07c51151f1bd6b59bf4675f4aacd7
Erik Tkal <eriktkal@gmail.com>
2023-02-04 16:34:04 -0500

    Update comments

--- src/PKITool.cpp
+++ src/PKITool.cpp
@@ -1,26 +1,26 @@
-//
-// PKITool.cpp : Defines the entry point for the console application.
-//
-//  PKITool
-//
-//  This version created by Erik Tkal on 2020.02.12.
-//  Copyright (c) 2018-2022 Erik Tkal. All rights reserved.
-//
-//  Built with OpenSSL 1.1.1n
-//  See buildOpenSSL script (places in ~/ssl and ~/ssl-debug)
-//
-//  ./Configure no-shared darwin64-x86_64-cc -mmacosx-version-min=10.12
-//  ./Configure no-shared enable-crypto-mdebug enable-crypto-mdebug-backtrace darwin64-x86_64-cc -mmacosx-version-min=10.12
-//
-//  ./Configure no-shared darwin64-x86_64-cc -mmacosx-version-min=10.12 --prefix=`pwd`/out-x86_64 -openssldir=`pwd`/out-x86_64
-//  make all
-//  make install
-//  make clean
-//  ./Configure no-shared darwin64-arm64-cc -mmacosx-version-min=10.12 --prefix=`pwd`/out-arm64 -openssldir=`pwd`/out-arm64
-//  make all
-//  make install
-//  make clean
-//
+/*
+ * PKITool.cpp : Defines the entry point for the console application.
+ *
+ * Copyright (c) 2023 Erik Tkal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include <stdio.h>
 #include <string.h>
