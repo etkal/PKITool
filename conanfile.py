d76408978e86a293ac2815b70803c44af996577b
Erik Tkal <eriktkal@gmail.com>
2026-05-21 11:56:30 -0400

    Update conan stuff

--- conanfile.py
+++ conanfile.py
@@ -2,10 +2,10 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 
-required_conan_version = ">=2.0.5"
+required_conan_version = ">=2.21.0"
 
 
-class ACMEClientLibrary(ConanFile):
+class PKITool(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     def configure(self):
@@ -15,8 +15,8 @@ class ACMEClientLibrary(ConanFile):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires("openssl/3.3.2")
-        self.requires("boost/1.82.0")
+        self.requires("openssl/3.4.5")
+        self.requires("boost/1.88.0")
 
     def generate(self):
         tc = CMakeToolchain(self)
